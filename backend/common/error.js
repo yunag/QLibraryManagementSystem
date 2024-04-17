16c49c4068789c1b8bfd55c73cf539bc6a21bb2d
Yunag <babushkin.ruslans@gmail.com>
2024-04-17 15:45:09 +0300

    revert(7dcedd4): move backend files to its own directory

--- backend/common/error.js
+++
@@ -1,8 +0,0 @@
-export function internalError(res) {
-  return res.status(500).json({ error: 'Internal server error' })
-}
-
-export function handleError(err, res) {
-  console.error(err)
-  internalError(res)
-}
--- backend/common/query.js
+++
@@ -1,8 +0,0 @@
-export function addWhereClause(baseQuery, conditions) {
-  if (!conditions || conditions.length === 0) {
-    return baseQuery
-  }
-
-  const whereClause = ` WHERE ${conditions.join(' AND ')}`
-  return baseQuery + whereClause
-}
